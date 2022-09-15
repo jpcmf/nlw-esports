3bf545d02167c0af399a56bb7a9083c4247ac23f
João Paulo <jpfricks@gmail.com>
2022-09-15 09:21:50 -0300

    chore: High Speed 🚅

---
+++ mobile/src/components/Loading/index.tsx
@@ -0,0 +1,15 @@
+import { ActivityIndicator, View } from 'react-native';
+import { THEME } from '../../theme';
+import { styles } from './styles';
+
+// interface Props {
+//   children?: React.ReactNode;
+// }
+
+export function Loading() {
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator color={THEME.COLORS.PRIMARY} size="large" />
+    </View>
+  );
+}
---
+++ mobile/src/screens/Home/index.tsx
@@ -0,0 +1,15 @@
+// import { ImageBackground } from 'react-native';
+import { Text, View } from 'react-native';
+import { styles } from './styles';
+
+// interface Props {
+//   children?: React.ReactNode;
+// }
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello</Text>
+    </View>
+  );
+}

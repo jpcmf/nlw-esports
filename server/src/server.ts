327ee4e86c74610a8e06f0819509e5491ee3f29c
João Paulo <jpfricks@gmail.com>
2022-09-19 22:33:13 -0300

    chore: To be continued  🥁

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,27 @@
 import express from "express";
 
+import { PrismaClient } from "@prisma/client";
+
 const app = express();
+const prisma = new PrismaClient();
+
+app.get("/games", async (request, response) => {
+  const games = await prisma.game.findMany();
+
+  return response.json(games);
+});
 
 app.get("/ads", (request, response) => {
+  return response.status(200).json([]);
+});
+
+app.post("/ads", (request, response) => {
+  return response.status(201).json([]);
+});
+
+app.get("/games/:id/ads", (request, response) => {
+  // const gameId = request.params.id;
+
   return response.json([
     {
       id: 1,
@@ -25,6 +44,12 @@ app.get("/ads", (request, response) => {
       name: "Ad 5",
     },
   ]);
+});
+
+app.get("/ads/:id/discord", (request, response) => {
+  // const adId = request.params.id;
+
+  return response.json([]);
 });
 
 app.listen(3333);
